38ea51397af06295c7564c1d75c3ab1a4483ce7f
notInvented <demogorgon-74@ya.ru>
2020-10-19 23:08:18 +0700

    change comments

--- lecture01/hw01.py
+++ lecture01/hw01.py
@@ -1,7 +1,8 @@
-#нахождение третей стороны через теорему косинуса
+# нахождение третей стороны через теорему косинуса
+
 import math
 
-#вводим значения
+# вводим значения
 a = float(input("enter the value of the first side: "))
 print("first side =", a)
 
@@ -11,7 +12,7 @@ print("second side =", b)
 c = float(input("enter the angle between the sides: "))
 print("angle betweed sides =", c)
 
-#вывод ответа
+# вывод ответа
 print("so, third side =",
 	math.sqrt(a**2 + b**2 - 2*a*b*math.cos(math.radians(c))))
 
