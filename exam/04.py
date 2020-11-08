961e4b503e22bfc01ee50769ce0d265b7d2db37b
notInvented <demogorgon-74@ya.ru>
2020-11-08 16:11:03 +0700

    exam #4_v2

--- exam/04.py
+++ exam/04.py
@@ -39,65 +39,52 @@ isroman(check[1])
 # перевод римских чисел в арабские
 def inarabic(value):
 	answer = 0
-	hundreds = {
-	'C': 100,
-	'CC': 200,
-	'CCC': 300,
-	'CD': 400,
-	'D': 500,
-	'DC': 600,
-	'DCC': 700,
-	'DCCC': 800,
-	'CM': 900,
-}
-	for any in sorted(hundreds, key=len, reverse=True):
-		if value.startswith(any):
-			if answer < 100:
-				answer += hundreds[any]
-				value = value[len(any):]
-			else:
-				print('\nincorrect input')
-				exit()
 
-	tens = {
-	'X': 10,
-	'XX': 20,
-	'XXX': 30,
-	'XL': 40,
-	'L': 50,
-	'LX': 60,
-	'LXX': 70,
-	'LXXX': 80,
-	'XC': 90,
-}
-	for any in sorted(tens, key=len, reverse=True):
-		if value.startswith(any):
-			if str(answer)[1] == '0':
-				answer += tens[any]
-				value = value[len(any):]
-			else:
-				print('\nincorrect input')
-				exit()
+	def discharge_inarabic(rom, discharge):	# rom от меньшего к большему, пример: CDM
+		nonlocal answer
+		nonlocal value
+		rom = list(rom)
+		X = ''
 
-	units = {
-	'I': 1,
-	'II': 2,
-	'III': 3,
-	'IV': 4,
-	'V': 5,
-	'VI': 6,
-	'VII': 7,
-	'VIII': 8,
-	'IX': 9,
-}
-	for any in sorted(units, key=len, reverse=True):
-		if value.startswith(any):
-			if str(answer)[-1] == '0':
-				answer += units[any]
-				value = value[len(any):]
-			else:
-				print('\nincorrect input')
-				exit()
+		def ent_check():
+			nonlocal rom
+			nonlocal value
+			nonlocal X
+			for any in rom:
+				if value.startswith(any):
+					X += value[0]
+					value = value[1:]
+
+
+		for _ in range(4):
+			ent_check()
+
+		if X.startswith(rom[0]):
+			if len(X) == 2:
+				if X[1] in rom[1]:
+					answer += discharge * 4
+				if X[1] in rom[2]:
+					answer += discharge * 9
+			if X in rom[0] * 3:
+				answer += discharge * len(X)
+
+		if X.startswith(rom[1]):
+			if len(X) == 1:
+				answer += discharge * 5
+			if 1 < len(X) <= 4 and X[1:] in rom[0] * 3:
+				answer += discharge * 5 + discharge * (len(X) - 1)
+
+	discharge_inarabic('CDM', 100)
+	discharge_inarabic('XLC', 10)
+	discharge_inarabic('IVX', 1)
+
+	if value:
+		print('\nincorrect input')
+		exit()
+
+	if not answer:
+		print('\nincorrect input')
+		exit()
 
 	return answer
 
@@ -106,49 +93,45 @@ arabic_answer = str(Fraction(inarabic(value[0]), inarabic(value[1]))).split('/')
 arabic_answer = list(map(int, arabic_answer))
 
 # go back in roman
-def inroman(value):
+def in_roman(value):
 	answer = ''
-	def vpadlu1(value, arab, rom):
+	def discharge_inroman():
+		nonlocal value
 		nonlocal answer
-		if value == arab:
-			answer += rom
-	if value > 99:
-		hundred = value - value % 100
-		vpadlu1(hundred, 100, 'C')
-		vpadlu1(hundred, 200, 'CC')
-		vpadlu1(hundred, 300, 'CCC')
-		vpadlu1(hundred, 400, 'CD')
-		vpadlu1(hundred, 500, 'D')
-		vpadlu1(hundred, 600, 'DC')
-		vpadlu1(hundred, 700, 'DCC')
-		vpadlu1(hundred, 800, 'DCCC')
-		vpadlu1(hundred, 900, 'CM')
-		value = value % 100
+		if len(str(value)) == 3:
+			rom = list('CDM')
+			discharge = 100
+		if len(str(value)) == 2:
+			rom = list('XLC')
+			discharge = 10
+		if len(str(value)) == 1:
+			rom = list('IVX')
+			discharge = 1
 
-	if value > 9:
-		ten = value - value % 10
-		vpadlu1(ten, 10, 'X')
-		vpadlu1(ten, 20, 'XX')
-		vpadlu1(ten, 30, 'XXX')
-		vpadlu1(ten, 40, 'XL')
-		vpadlu1(ten, 50, 'L')
-		vpadlu1(ten, 60, 'LX')
-		vpadlu1(ten, 70, 'LXX')
-		vpadlu1(ten, 80, 'LXX')
-		vpadlu1(ten, 90, 'XC')
-		value = value % 10
+		if 5 <= value // discharge <= 8:
+			answer += rom[1]
+			if 6 <= value // discharge <= 8:
+				keks = value // discharge - 5
+				answer += rom[0] * keks
 
-	vpadlu1(value, 1, 'I')
-	vpadlu1(value, 2, 'II')
-	vpadlu1(value, 3, 'III')
-	vpadlu1(value, 4, 'IV')
-	vpadlu1(value, 5, 'V')
-	vpadlu1(value, 6, 'VI')
-	vpadlu1(value, 7, 'VII')
-	vpadlu1(value, 8, 'VIII')
-	vpadlu1(value, 9, 'IX')
+		if 1 <= value // discharge <= 3:
+			answer += rom[0] * (value // discharge)
+
+		if value // discharge == 4:
+			answer += rom[0]
+			answer += rom[1]
+
+		if value // discharge == 9:
+			answer += rom[0]
+			answer += rom[2]
+
+		value = value - (value // discharge * discharge)
+
+	for_range = len(str(value))
+	for _ in range(for_range):
+		discharge_inroman()
 
 	return answer
 
 # ответыч
-print(f'\nответ: {inroman(arabic_answer[0])}/{inroman(arabic_answer[1])}')
+print(f'\nответ: {in_roman(arabic_answer[0])}/{in_roman(arabic_answer[1])}')
